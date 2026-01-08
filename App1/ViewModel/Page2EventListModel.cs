9289510fb81c7d46de020f9dcf0b8dc7ba181d20
foxseSReal <207896979+foxseSReal@users.noreply.github.com>
2026-01-08 22:20:07 +0300

    NuGet Paketi eklendi. ViewModellerin usingleri ayarlandı.

--- App1/ViewModel/Page2EventListModel.cs
+++ App1/ViewModel/Page2EventListModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace App1.ViewModel
 {
